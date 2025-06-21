fc6cdc9916ac08b105262d2cda127959e8c7dad7
IzioDev <romain@izio.fr>
2025-06-21 04:58:19 +0200

    fix(send-message): fee estimation

--- src/utils/network-display.ts
+++ src/utils/network-display.ts
@@ -1,12 +1,9 @@
-import { NetworkType } from "kaspa-wasm";
-
 export enum KasiaNetwork {
   MAINNET = "mainnet",
   TESTNET_10 = "testnet-10",
 }
 
 export const getDisplayableNetworkFromNetworkString = (network: string) => {
-  console.log(network);
   if (network === KasiaNetwork.MAINNET) {
     return "Mainnet";
   }
@@ -17,31 +14,3 @@ export const getDisplayableNetworkFromNetworkString = (network: string) => {
 
   return "Unknown";
 };
-
-// export const getDisplayableNetworkFromNetworkType = (
-//   networkType?: NetworkType | null
-// ) => {
-//   console.log(networkType);
-
-//   if (networkType === NetworkType.Mainnet) {
-//     return "Mainnet";
-//   }
-
-//   if (networkType === NetworkType.Testnet) {
-//     return "Testnet";
-//   }
-
-//   return "Unknown";
-// };
-
-// export const getNetworkTypeFromNetworkString = (network: string) => {
-//   if (network === KasiaNetwork.MAINNET) {
-//     return NetworkType.Mainnet;
-//   }
-
-//   if (network === KasiaNetwork.TESTNET_10) {
-//     return NetworkType.Testnet;
-//   }
-
-//   return NetworkType.Mainnet;
-// };
