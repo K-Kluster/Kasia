1b94561b6008177057550ce00fbd32f5bd21944d
HocusLocusT <182385655+HocusLocusTee@users.noreply.github.com>
2025-06-22 19:45:09 +1000

    chore: cleak kasware from globals

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,7 +1,2 @@
-export declare global {
-  // @TODO: properly type this, is still relevant
-  interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    kasware: any;
-  }
+declare global {
 }
