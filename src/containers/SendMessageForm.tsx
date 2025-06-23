7ad28335e07abcdd19d483cb2b73031409ddb76f
Auzghosty <50971633+AuzGhosty@users.noreply.github.com>
2025-06-23 22:01:42 +1000

    Refactor: Move BackwardsKIcon to shared components
    - Created src/components/icons/BackwardsKIcon.tsx for reusability
    - Updated SendMessageForm.tsx to import from new location

--- src/containers/SendMessageForm.tsx
+++ src/containers/SendMessageForm.tsx
@@ -9,21 +9,7 @@ import { formatKasAmount } from "../utils/format";
 import { createWithdrawTransaction } from "../service/account-service";
 import clsx from "clsx";
 import { PaperClipIcon, PaperAirplaneIcon } from "@heroicons/react/24/outline";
-
-// Backwards K icon component for Kaspa
-const BackwardsKIcon: FC<{ className?: string }> = ({ className }) => (
-  <svg
-    className={className}
-    fill="currentColor"
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path
-      d="M8 4v16h2v-6.5l1.5-1.5L16 18h2.5l-5-7 4.5-7H15.5L12 8.5V4H8z"
-      transform="scale(-1,1) translate(-24,0)"
-    />
-  </svg>
-);
+import { BackwardsKIcon } from "../components/icons/BackwardsKIcon";
 import { toast } from "../utils/toast";
 
 type SendMessageFormProps = unknown;
