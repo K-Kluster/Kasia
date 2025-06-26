5e9102e9df0ad4b619f3294856d6e76144792043
Romain Billot <romainbillot3009@gmail.com>
2025-06-26 03:15:11 +0200

    ui(mobile): add splash and fix settings page layout (#74)
    * feat(splash): introduce a splashscreen
    
    * ui(settings): fix layout on mobile
    
    * review: move splash component to layout directory
    
    ---------
    
    Co-authored-by: IzioDev <romain@izio.fr>

---
+++ src/components/Layout/Splash.tsx
@@ -0,0 +1,21 @@
+// WARNING: this file CANNOT use any imports from the app code, it must be a standalone file
+import { FC } from "react";
+import { ArrowPathIcon } from "@heroicons/react/24/solid";
+
+export const SplashScreen: FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-6">
+      <img src="/kasia-logo.png" alt="Kasia Logo" className="w-32 h-32" />
+      <div className="flex flex-col items-center gap-4">
+        <h1 className="text-center text-4xl font-bold mt-4">
+          Kasia: Encrypted Messaging Platform
+        </h1>
+        <p className="text-lg mt-2">No fees, only freedom.</p>
+      </div>
+      <div className="mt-4 flex items-center gap-2">
+        <ArrowPathIcon className="animate-spin h-8 w-8 text-kas-primary" />
+        Loading Kasia SDKs...
+      </div>
+    </div>
+  );
+};
