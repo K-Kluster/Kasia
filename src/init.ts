b575610c6f1a8ff5c313ba270b7e36454a116992
HocusLocusT <182385655+HocusLocusTee@users.noreply.github.com>
2025-07-31 20:23:18 +1000

    refactor: change splash to ts from react

--- src/init.ts
+++ src/init.ts
@@ -3,24 +3,33 @@ import initCipherWasm from "cipher";
 import "./utils/debug-commands"; // Import debug commands
 import "./utils/logging";
 import { createRoot, type Root } from "react-dom/client";
-import { SplashScreen } from "./components/Layout/Splash";
+import {
+  mountSplashScreen,
+  unmountSplashScreen,
+} from "./components/Layout/Splash.ts";
 import "./index.css";
 import { client as indexerClient } from "./service/indexer/generated/client.gen";
 
 let root: Root;
+let splashElement: HTMLElement;
 
 // load wasm entry point, and lazy load sub-module so we don't have to worry
 // about ordering of wasm module initialization
 export async function boot() {
   const container = document.getElementById("root")!;
-  root = createRoot(container);
-  root.render(await SplashScreen({}));
+
+  // mount plain js splash screen
+  splashElement = mountSplashScreen(container);
 
   await Promise.all([initKaspaWasm(), initCipherWasm()]);
 
   initConsolePanicHook();
 
   console.log("Kaspa SDK initialized successfully");
+
+  // unmount splash screen and create react root
+  unmountSplashScreen(splashElement);
+  root = createRoot(container);
 
   // lazy load main
   const { loadApplication } = await import("./main");
